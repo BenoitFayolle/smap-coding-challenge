03a803406debca808f97b542f44daa3d750a9a4a
Benoit Fayolle <benoit.fayolle53@gmail.com>
2018-05-23 15:48:05 +0100

    edit urls.py

--- dashboard/consumption/urls.py
+++ dashboard/consumption/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.summary),
     url(r'^summary/', views.summary),
-    #url('detail/<int:id_request>', views.detail,name="detail"),
     path('detail/<int:id_request>', views.detail, name='detail')
 ]
